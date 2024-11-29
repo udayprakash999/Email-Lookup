0c61bc606d828ea043fc774a736edc77a41b8fb7
Uday Prakash Gupta <128396030+udayprakash999@users.noreply.github.com>
2024-11-29 23:23:48 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ submitBtn.addEventListener("click", async (e) => {
     e.preventDefault()
     console.log("Clicked!")
     resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
-    let key = {$apikeys}
+    let key = process.env.apikeys
     let email = document.getElementById("username").value 
     let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
     let res = await fetch(url)
